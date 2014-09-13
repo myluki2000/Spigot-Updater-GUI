9eb694b83222a24573c44bd5bb1486619a9fe5d5
myluki2000 <lukas636@live.com>
2014-09-13 22:36:27 +0200

    Filetype checker

--- Spigot Updater GUI/Form1.vb
+++ Spigot Updater GUI/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Option Strict On
 
 Imports MetroSuite
+Imports System.IO
 
 Public Class MainForm
     Inherits MetroForm
@@ -10,20 +11,13 @@ Public Class MainForm
     Dim PatchJar As String = ""
     Dim OutputJar As String = ""
 
+
     Private Sub MainForm_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
         Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
-        For Each path In files
-            If PatcherJar = "" Then
-                PatcherJar = path
-                HelpLabel.Text = "Drop the spigot-1649.jar here"
-            ElseIf SpigotJar = "" Then
-                SpigotJar = path
-                HelpLabel.Text = "Now drop the patch.bps file here"
-            ElseIf PatchJar = "" Then
-                PatchJar = path
-                HelpLabel.Text = "Now select a location for the patched jar"
-                DropImage.Visible = False
 
+        For Each filepath In files
+            If Path.GetExtension(filepath) = ".jar" OrElse Path.GetExtension(filepath) = ".bps" Then
+                DroppedFile(e)
             End If
         Next
     End Sub
@@ -54,5 +48,22 @@ Public Class MainForm
         Process.Start("cmd", "/c java -jar " + PatcherJar + " " + SpigotJar + " " + PatchJar + " " + OutputJar)
 
         HelpLabel.Text = "Your file has been patched!"
+    End Sub
+
+    Private Sub DroppedFile(e As DragEventArgs)
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+        For Each filepath In files
+            If PatcherJar = "" AndAlso Path.GetExtension(filepath) = ".jar" Then
+                PatcherJar = filepath
+                HelpLabel.Text = "Drop the spigot-1649.jar here"
+            ElseIf SpigotJar = "" Then
+                SpigotJar = filepath
+                HelpLabel.Text = "Now drop the patch.bps file here"
+            ElseIf PatchJar = "" Then
+                PatchJar = filepath
+                HelpLabel.Text = "Now select a location for the patched jar"
+                DropImage.Visible = False
+            End If
+        Next
     End Sub
 End Class
