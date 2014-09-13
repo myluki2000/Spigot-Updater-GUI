1d3c7d91614fa3b406d7f06c746b5ac6e80bccd4
myluki2000 <lukas636@live.com>
2014-09-13 20:02:05 +0200

    Drop Files #2 + Save Output #1

--- Spigot Updater GUI/Form1.Designer.vb
+++ Spigot Updater GUI/Form1.Designer.vb
@@ -23,9 +23,12 @@ Partial Class MainForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Dim MainColorScheme2 As MetroSuite.MetroButton.MainColorScheme = New MetroSuite.MetroButton.MainColorScheme()
         Me.MetroControlBox1 = New MetroSuite.MetroControlBox()
         Me.DropImage = New System.Windows.Forms.PictureBox()
         Me.HelpLabel = New System.Windows.Forms.Label()
+        Me.SaveButton = New MetroSuite.MetroButton()
+        Me.SaveJarDialog = New System.Windows.Forms.SaveFileDialog()
         CType(Me.DropImage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,7 +38,7 @@ Partial Class MainForm
         Me.MetroControlBox1.Dock = System.Windows.Forms.DockStyle.Right
         Me.MetroControlBox1.FormHeight = 0
         Me.MetroControlBox1.FormWidth = 0
-        Me.MetroControlBox1.Location = New System.Drawing.Point(-110, 0)
+        Me.MetroControlBox1.Location = New System.Drawing.Point(217, 0)
         Me.MetroControlBox1.Name = "MetroControlBox1"
         Me.MetroControlBox1.Size = New System.Drawing.Size(110, 26)
         Me.MetroControlBox1.TabIndex = 0
@@ -53,33 +56,57 @@ Partial Class MainForm
         'HelpLabel
         '
         Me.HelpLabel.AllowDrop = True
-        Me.HelpLabel.AutoSize = True
         Me.HelpLabel.Font = New System.Drawing.Font("Calibri", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.HelpLabel.Location = New System.Drawing.Point(36, 334)
         Me.HelpLabel.Name = "HelpLabel"
-        Me.HelpLabel.Size = New System.Drawing.Size(255, 24)
+        Me.HelpLabel.Size = New System.Drawing.Size(255, 50)
         Me.HelpLabel.TabIndex = 2
         Me.HelpLabel.Text = "Drop the spigot-1649.jar here"
+        Me.HelpLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'SaveButton
+        '
+        MainColorScheme2.BackgroundColorHover = System.Drawing.Color.FromArgb(CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer), CType(CType(225, Byte), Integer))
+        MainColorScheme2.BackgroundColorNormal = System.Drawing.Color.FromArgb(CType(CType(215, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(215, Byte), Integer))
+        MainColorScheme2.BackgroundColorPressed = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
+        MainColorScheme2.BorderColor = System.Drawing.Color.FromArgb(CType(CType(210, Byte), Integer), CType(CType(210, Byte), Integer), CType(CType(210, Byte), Integer))
+        MainColorScheme2.BorderColorHover = System.Drawing.Color.FromArgb(CType(CType(21, Byte), Integer), CType(CType(159, Byte), Integer), CType(CType(79, Byte), Integer))
+        MainColorScheme2.BorderColorPressed = System.Drawing.Color.FromArgb(CType(CType(215, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(215, Byte), Integer))
+        Me.SaveButton.ColorScheme = MainColorScheme2
+        Me.SaveButton.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.SaveButton.ForeColor = System.Drawing.Color.Black
+        Me.SaveButton.Location = New System.Drawing.Point(100, 177)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(128, 40)
+        Me.SaveButton.TabIndex = 3
+        Me.SaveButton.Text = "Save"
+        '
+        'SaveJarDialog
+        '
+        Me.SaveJarDialog.FileName = "output.jar"
+        Me.SaveJarDialog.Filter = "jar File|*.jar"
         '
         'MainForm
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(0, 0)
+        Me.ClientSize = New System.Drawing.Size(327, 393)
         Me.Controls.Add(Me.HelpLabel)
         Me.Controls.Add(Me.MetroControlBox1)
         Me.Controls.Add(Me.DropImage)
+        Me.Controls.Add(Me.SaveButton)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "MainForm"
         Me.Text = "Form1"
         CType(Me.DropImage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents MetroControlBox1 As MetroSuite.MetroControlBox
     Friend WithEvents DropImage As System.Windows.Forms.PictureBox
     Friend WithEvents HelpLabel As System.Windows.Forms.Label
+    Friend WithEvents SaveButton As MetroSuite.MetroButton
+    Friend WithEvents SaveJarDialog As System.Windows.Forms.SaveFileDialog
 
 End Class
--- Spigot Updater GUI/Form1.vb
+++ Spigot Updater GUI/Form1.vb
@@ -17,6 +17,9 @@ Public Class MainForm
                 HelpLabel.Text = "Now drop the patch.bps file"
             ElseIf PatchJar = "" Then
                 PatchJar = path
+                HelpLabel.Text = "Now select a place to save the output jar"
+                DropImage.Visible = False
+
             End If
         Next
     End Sub
@@ -29,5 +32,17 @@ Public Class MainForm
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Size = New Size(327, 393)
+    End Sub
+
+    Private Sub SaveButton_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
+        SaveJarDialog.ShowDialog()
+
+        OutputJar = SaveJarDialog.FileName
+
+        SaveButton.Hide()
+    End Sub
+
+    Private Sub PatchFile()
+
     End Sub
 End Class
