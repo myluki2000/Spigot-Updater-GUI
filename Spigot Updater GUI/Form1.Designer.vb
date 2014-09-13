b4a26a8d6811ed52d78a5372d996f897113a8432
myluki2000 <lukas636@live.com>
2014-09-13 19:50:27 +0200

    Drop Files #1

--- Spigot Updater GUI/Form1.Designer.vb
+++ Spigot Updater GUI/Form1.Designer.vb
@@ -24,45 +24,62 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
         Me.MetroControlBox1 = New MetroSuite.MetroControlBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DropImage = New System.Windows.Forms.PictureBox()
+        Me.HelpLabel = New System.Windows.Forms.Label()
+        CType(Me.DropImage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MetroControlBox1
         '
+        Me.MetroControlBox1.AllowDrop = True
         Me.MetroControlBox1.Dock = System.Windows.Forms.DockStyle.Right
         Me.MetroControlBox1.FormHeight = 0
         Me.MetroControlBox1.FormWidth = 0
-        Me.MetroControlBox1.Location = New System.Drawing.Point(352, 0)
+        Me.MetroControlBox1.Location = New System.Drawing.Point(-110, 0)
         Me.MetroControlBox1.Name = "MetroControlBox1"
         Me.MetroControlBox1.Size = New System.Drawing.Size(110, 26)
         Me.MetroControlBox1.TabIndex = 0
         '
-        'PictureBox1
+        'DropImage
         '
-        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(-2, 88)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(464, 363)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 1
-        Me.PictureBox1.TabStop = False
+        Me.DropImage.Image = CType(resources.GetObject("DropImage.Image"), System.Drawing.Image)
+        Me.DropImage.Location = New System.Drawing.Point(0, 61)
+        Me.DropImage.Name = "DropImage"
+        Me.DropImage.Size = New System.Drawing.Size(327, 270)
+        Me.DropImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.DropImage.TabIndex = 1
+        Me.DropImage.TabStop = False
+        '
+        'HelpLabel
+        '
+        Me.HelpLabel.AllowDrop = True
+        Me.HelpLabel.AutoSize = True
+        Me.HelpLabel.Font = New System.Drawing.Font("Calibri", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HelpLabel.Location = New System.Drawing.Point(36, 334)
+        Me.HelpLabel.Name = "HelpLabel"
+        Me.HelpLabel.Size = New System.Drawing.Size(255, 24)
+        Me.HelpLabel.TabIndex = 2
+        Me.HelpLabel.Text = "Drop the spigot-1649.jar here"
         '
         'MainForm
         '
         Me.AllowDrop = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(462, 540)
+        Me.ClientSize = New System.Drawing.Size(0, 0)
+        Me.Controls.Add(Me.HelpLabel)
         Me.Controls.Add(Me.MetroControlBox1)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.DropImage)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "MainForm"
         Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DropImage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents MetroControlBox1 As MetroSuite.MetroControlBox
-    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents DropImage As System.Windows.Forms.PictureBox
+    Friend WithEvents HelpLabel As System.Windows.Forms.Label
 
 End Class
--- Spigot Updater GUI/Form1.vb
+++ Spigot Updater GUI/Form1.vb
@@ -1,13 +1,33 @@
-﻿Imports MetroSuite
+﻿Option Strict On
+
+Imports MetroSuite
 
 Public Class MainForm
     Inherits MetroForm
 
-    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+    Dim SpigotJar As String = ""
+    Dim PatchJar As String = ""
+    Dim OutputJar As String = ""
 
+    Private Sub MainForm_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+        For Each path In files
+            If SpigotJar = "" Then
+                SpigotJar = path
+                HelpLabel.Text = "Now drop the patch.bps file"
+            ElseIf PatchJar = "" Then
+                PatchJar = path
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Size = New Size(500, 500)
+        Me.Size = New Size(327, 393)
     End Sub
 End Class
